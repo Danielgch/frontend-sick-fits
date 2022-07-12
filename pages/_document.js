407fafdb5773d7d626b030b45d7d1cc62790336c
Daniel Giraldo <danielgiraldo@gmail.com>
2022-07-11 22:26:33 -0500

    rmv console logs

--- pages/_document.js
+++ pages/_document.js
@@ -4,14 +4,11 @@ import { ServerStyleSheet } from 'styled-components';
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
-    const page = renderPage((App) => (props) =>
-      sheet.collectStyles(<App {...props} />)
+    const page = renderPage(
+      (App) => (props) => sheet.collectStyles(<App {...props} />)
     );
     const styleTags = sheet.getStyleElement();
-    console.log(
-      '🚀 ~ file: _document.js ~ line 11 ~ MyDocument ~ getInitialProps ~ styleTags',
-      styleTags
-    );
+    
     return { ...page, styleTags };
   }
 
--- pages/update.js
+++ pages/update.js
@@ -1,7 +1,6 @@
 import UpdateProduct from '../components/UpdateProduct';
 
 export default function UpdatePage({ query }) {
-  console.log('🚀 ~ file: update.js ~ line 4 ~ UpdatePage ~ query', query);
   return (
     <div>
       <UpdateProduct id={query.id} />
